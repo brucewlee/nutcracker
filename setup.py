6c0e68d04a8690f4d161462c827112916da14934
Bruce Lee <brucelws@seas.upenn.edu>
2024-02-24 14:59:10 -0500

    workflow

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import find_packages
 # python setup.py sdist
 # python -m twine upload dist/*
 # pip install -e .
+# git tag -a "0.0.1a12" -m "pypi workflow revamp"
+# git push --tags    
 setup(
     name = 'nutcracker-py',  
-    version='{{VERSION_PLACEHOLDER}}',,
+    version='{{VERSION_PLACEHOLDER}}',
     description = 'streamline LLM evaluation',
     author = 'Bruce W. Lee',
     author_email = 'bruce@walnutresearch.com', 
